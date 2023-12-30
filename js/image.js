2d4157e8926ae4aaff932ba296dac42678a73693
henrietta2243 <144639181+henrietta2243@users.noreply.github.com>
2023-12-30 23:20:16 +0700

    Update image.js

--- js/image.js
+++ js/image.js
@@ -1,18 +1,12 @@
-// Get all image elements with the class "hide" and "skeleton"
-var images = document.querySelectorAll(' img.hide');
+var images = document.querySelectorAll(' .hide');
 
-// Function to remove classes after image is loaded
 function removeClassesAfterLoad(image) {
-  // Remove the 'hide' class
   image.classList.remove("hide");
-  // Remove the 'skeleton' class from the parent container
   image.parentElement.classList.remove("skeleton");
 };
 
-// Add 'load' event listener for each image
 images.forEach(function(image) {
   image.addEventListener("load", function() {
-    // Call the function to remove classes after image is loaded
     removeClassesAfterLoad(image);
   });
   image.addEventListener("error", function(replace) {
