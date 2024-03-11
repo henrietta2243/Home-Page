3154d75f642a50af599191fbb1b0292a69e8a1ee
henrietta2243 <alya.p904@gmail.com>
2024-03-11 21:27:13 +0700

    update post and js

--- js/post.js
+++ js/post.js
@@ -23,3 +23,29 @@ themesDivs.forEach(div => {
         img.addEventListener("error", notFound)
     }
 })
+
+const relDivs = document.querySelectorAll(".rel-image")
+
+relDivs.forEach(div => {
+    const img = div.querySelector("img")
+
+    function loaded(){
+        div.classList.add("loaded")
+    }
+
+    if (img.complete) {
+        loaded()
+    } else {
+        img.addEventListener("load", loaded)
+    }
+
+    function notFound() {
+        div.style.backgroundImage='url("/Home-Page/img/404.png")';
+    }
+
+    if (img.error){
+        notFound()
+    } else {
+        img.addEventListener("error", notFound)
+    }
+})
