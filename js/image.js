33cd37005da2dddcf5c7097c81a142ac8f8144cf
henrietta2243 <alya.p904@gmail.com>
2024-03-18 18:37:11 +0700

    #

--- js/image.js
+++ js/image.js
@@ -24,37 +24,29 @@ themesDivs.forEach(div => {
   }
 })
 
-const btn1s = document.getElementById("btn1")
-btn1s.addEventListener("click", function(){
-  const img1s = document.getElementById("img1")
-  img1s.classList.remove("nsfw")
-  btn1s.classList.remove("show")
+const openNsfwBtn = document.querySelectorAll('[data-nsfw-target]')
+const remNuxtBtn = document.querySelectorAll('[data-btn-rem]')
+
+openNsfwBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        const nsfw = document.querySelector(button.dataset.nsfwTarget)
+        openNsfw(nsfw)
+    })
 })
 
-const btn2s = document.getElementById("btn2")
-btn2s.addEventListener("click", function(){
-  const img2s = document.getElementById("img2")
-  img2s.classList.remove("nsfw")
-  btn2s.classList.remove("show")
+remNuxtBtn.forEach(button => {
+    button.addEventListener('click', () =>{
+        const nuxt = document.querySelector(button.dataset.btnRem)
+        remNuxt(nuxt)
+    })
 })
 
-const btn3s = document.getElementById("btn3")
-btn3s.addEventListener("click", function(){
-  const img3s = document.getElementById("img3")
-  img3s.classList.remove("nsfw")
-  btn3s.classList.remove("show")
-})
+function openNsfw(nsfw){
+    if (nsfw == null) return
+    nsfw.classList.remove('nsfw')
+}
 
-const btn4s = document.getElementById("btn4")
-btn4s.addEventListener("click", function(){
-  const img4s = document.getElementById("img4")
-  img4s.classList.remove("nsfw")
-  btn4s.classList.remove("show")
-})
-
-const btn5 = document.getElementById('btn5')
-btn5.addEventListener('click', function(){
-  const img5 = document.getElementById('img5')
-  img5.classList.remove("nsfw")
-  btn5.classList.remove("show")
-})
+function remNuxt(nuxt){
+    if (nuxt == null) return
+    nuxt.classList.remove('show')
+}
--- js/post.js
+++ js/post.js
@@ -50,16 +50,68 @@ relDivs.forEach(div => {
     }
 })
 
-const btn1s = document.getElementById("btn1")
-btn1s.addEventListener("click", function(){
-    const img1s = document.getElementById("img1")
-    img1s.classList.remove("nsfw")
-    btn1s.classList.remove("show")
+const openNsfwBtn = document.querySelectorAll('[data-nsfw-target]')
+const remNuxtBtn = document.querySelectorAll('[data-btn-rem]')
+
+openNsfwBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        const nsfw = document.querySelector(button.dataset.nsfwTarget)
+        openNsfw(nsfw)
+    })
 })
 
-const btn2s = document.getElementById("btn2")
-btn2s.addEventListener("click", function(){
-    const img2s = document.getElementById("img2")
-    img2s.classList.remove("nsfw")
-    btn2s.classList.remove("show")
+remNuxtBtn.forEach(button => {
+    button.addEventListener('click', () =>{
+        const nuxt = document.querySelector(button.dataset.btnRem)
+        remNuxt(nuxt)
+    })
 })
+
+function openNsfw(nsfw){
+    if (nsfw == null) return
+    nsfw.classList.remove('nsfw')
+    nsfw.classList.remove('nsfw1')
+}
+
+function remNuxt(nuxt){
+    if (nuxt == null) return
+    nuxt.classList.remove('show')
+    nuxt.classList.add('nuxt1')
+}
+
+const openModalBtn = document.querySelectorAll('[data-modal-target]')
+const closeModalBtn = document.querySelectorAll('[data-close-btn]')
+const overlay = document.getElementById('overlay')
+
+openModalBtn.forEach(button => {
+    button.addEventListener('click', () =>{
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+closeModalBtn.forEach(button => {
+    button.addEventListener('click', () =>{
+        const modal = button.closest('.mdls')
+        closeModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modal = document.querySelectorAll('.active')
+    modal.forEach( modal => {
+        closeModal(modal)
+    })
+})
+
+function openModal(modal){
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal){
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
